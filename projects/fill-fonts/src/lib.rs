ab8f0cadb10d9c8031ee475c8c3c2f7cff3afff9
Aster <galaster@foxmail.com>
2023-03-01 20:53:30 +0800

    Define the font canvas

--- projects/fill-fonts/src/lib.rs
+++ projects/fill-fonts/src/lib.rs
@@ -1,5 +1,38 @@
+use std::ops::Div;
+
 use fontdue::{Font, FontSettings};
 use image::RgbaImage;
+
+pub struct FontCanvas {
+    pub canvas: RgbaImage,
+}
+
+impl FontCanvas {
+    pub fn new(size: usize) -> Self {
+        Self {
+            canvas: RgbaImage::new(size as u32, size as u32),
+        }
+    }
+    pub fn size(&self) -> usize {
+        self.canvas.width() as usize
+    }
+    pub fn draw(&mut self, c: char, font: &Font) {
+        let size = self.size();
+        let (metrics, bitmap) = font.rasterize(c, self.size() as f32);
+        for x in 0..metrics.width {
+            for y in 0..metrics.height {
+                let px = x;
+                let py = y;
+                let index = y * metrics.width + x;
+                let alpha = bitmap[index];
+                self.canvas.put_pixel(px as u32, py as u32, image::Rgba([255, 255, 255, alpha]));
+            }
+        }
+    }
+    pub fn save(&self, path: &str) {
+        self.canvas.save(path).unwrap();
+    }
+}
 
 #[test]
 fn test() {
@@ -7,18 +40,20 @@ fn test() {
     let font = include_bytes!(r"F:\project-a\client\ProjectA\Assets\Art\Fonts\SourceHanSansSC-Regular.otf") as &[u8];
 // Parse it into the font type.
     let font = Font::from_bytes(font, FontSettings::default()).unwrap();
-// Rasterize and get the layout metrics for the letter 'g' at 17px.
-    let (metrics, bitmap) = font.rasterize('g', 144.0);
 
-    let mut canvas = RgbaImage::new(144, 144);
-    for x in 0..metrics.width {
-        for y in 0..metrics.height {
-            let px = metrics.xmin + x;
-            let py = metrics.ymin + y;
-            let index = (y * metrics.width + x) as usize;
-            let alpha = bitmap[index];
-            canvas.put_pixel(px as u32, py as u32, image::Rgba([255, 255, 255, alpha]));
-        }
+    let mut canvas = FontCanvas::new(144);
+    canvas.draw('g', &font);
+    canvas.save("test.png");
+}
+
+fn alignment_center(size: u32, min: i32, index: usize) -> u32 {
+    let mut x = min + index as i32;
+    x += size.div(2) as i32;
+    if x <= 0 {
+        0
+    } else if x >= size as i32 {
+        size - 1
+    } else {
+        x as u32
     }
-    canvas.save("test.png").unwrap();
 }
