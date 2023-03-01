2cc55e340356df5dc2ef3dc3d6922d96b6572fb9
Aster <galaster@foxmail.com>
2023-03-01 20:36:39 +0800

    Render text to image

--- projects/fill-fonts/src/lib.rs
+++ projects/fill-fonts/src/lib.rs
@@ -1,23 +1,24 @@
-
+use fontdue::{Font, FontSettings};
+use image::RgbaImage;
 
 #[test]
 fn test() {
-    let font = SystemSource::new()
-        .select_by_postscript_name("ArialMT")
-        .unwrap()
-        .load()
-        .unwrap();
+// Read the font data.
+    let font = include_bytes!(r"F:\project-a\client\ProjectA\Assets\Art\Fonts\SourceHanSansSC-Regular.otf") as &[u8];
+// Parse it into the font type.
+    let font = Font::from_bytes(font, FontSettings::default()).unwrap();
+// Rasterize and get the layout metrics for the letter 'g' at 17px.
+    let (metrics, bitmap) = font.rasterize('g', 144.0);
 
-    let glyph_id = font.glyph_for_char('A').unwrap();
-    let mut canvas = Canvas::new(&Size2D::new(32, 32), Format::A8);
-
-    font.rasterize_glyph(
-        &mut canvas,
-        glyph_id,
-        32.0,
-        &Point2D::new(0.0, 32.0),
-        HintingOptions::None,
-        RasterizationOptions::GrayscaleAa,
-    )
-        .unwrap();
+    let mut canvas = RgbaImage::new(144, 144);
+    for x in 0..metrics.width {
+        for y in 0..metrics.height {
+            let px = metrics.xmin + x;
+            let py = metrics.ymin + y;
+            let index = (y * metrics.width + x) as usize;
+            let alpha = bitmap[index];
+            canvas.put_pixel(px as u32, py as u32, image::Rgba([255, 255, 255, alpha]));
+        }
+    }
+    canvas.save("test.png").unwrap();
 }
